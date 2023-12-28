e705d3bd028dcd56a244b3a02cc47b9e9e5b0aef
Flidix <marko.lazarchuk@gmail.com>
2023-12-28 20:33:29 +0200

    feat: fix bugs

--- src/group/group.module.ts
+++ src/group/group.module.ts
@@ -1,16 +1,12 @@
 import { Module } from '@nestjs/common';
 
-import { ChatModule } from 'src/chat/chat.module';
-
 import { GroupController } from './group.controller';
 
 import { GroupService } from './group.service';
-import { ChatService } from 'src/chat/chat.service';
-import { MessagesService } from 'src/messages/messages.service';
 
 @Module({
   controllers: [GroupController],
-  providers: [GroupService, ChatService, MessagesService],
-  imports: [ChatModule],
+  providers: [GroupService],
+  imports: [],
 })
 export class GroupModule {}
